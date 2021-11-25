216f3fe1934e38e9f38d87de998dbb1112047682
Alex Lohr <alex.lohr@logmein.com>
2021-11-25 11:38:10 +0100

    version bump; dom improvements

--- src/register-dom.ts
+++ src/register-dom.ts
@@ -1,33 +1,6 @@
 export const registerDom = (dom: any) => {
-  const {
-    window,
-    document,
-    Node,
-    HTMLElement,
-    SVGElement,
-    DOMParser,
-    requestAnimationFrame,
-    cancelAnimationFrame,
-    navigator,
-    localStorage,
-    sessionStorage
-  } = dom;
-  Object.assign(globalThis, {
-    window,
-    document,
-    Node,
-    HTMLElement,
-    SVGElement,
-    DOMParser,
-    requestAnimationFrame,
-    cancelAnimationFrame,
-    navigator,
-    localStorage,
-    sessionStorage,
-    Event: window.Event
-  });
   Object.getOwnPropertyNames(dom).forEach((name) => {
-    if (/^HTML\w+Element$/.test(name)) {
+    if (name === 'Event' || !globalThis.hasOwnProperty(name)) {
       Object.assign(globalThis, { [name]: dom[name] });
     }
   });
